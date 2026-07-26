62309911305b1a3b34559bf28dc6c94e537af831
Siraj Elbadri <srjbadri2008@gmail.com>
2026-07-27 01:51:08 +0200

    feat: create users and password reset tokens database tables

--- database/migrations/2026_01_01_000000_create_users_table.php
+++ database/migrations/2026_01_01_000000_create_users_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+        });
+
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
